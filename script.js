1dec507d75ca0968b9e732fa79dc62540fe6d539
sameer-shaikhdev <130742021+sameer-shaikhdev@users.noreply.github.com>
2023-06-20 06:29:18 +0000

    ex04: adding textarea and taking value from user and consoling it.

--- script.js
+++ script.js
@@ -5,12 +5,26 @@
 // alert("welcome to the website "+ user);
 
 
-// ex03
+// // ex03
 
-var primaryButton= document.querySelector("#primary-button");
+// var primaryButton= document.querySelector("#primary-button");
 
-function clickEventHandler(){
+// function clickEventHandler(){
+//   console.log("clicked");
+// };
+
+// primaryButton.addEventListener("click", clickEventHandler);
+
+
+//ex 04
+
+var primaryButton = document.querySelector("#primary-button");
+var primaryTextArea = document.querySelector("#txt-area");        //text area refernce in javascript
+
+function clickEventHandler() {
   console.log("clicked");
+  console.log("input", primaryTextArea.value);                    //taking input from the textarea onclick and consoling it;
 };
 
 primaryButton.addEventListener("click", clickEventHandler);
+
